8d2db9f0cecaec8be5487884683abe71cefefda1
Andri-Almengor <120224177+Andri-Almengor@users.noreply.github.com>
2026-04-29 14:35:42 -0600

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-import 'react-native-get-random-values';
-import { Buffer } from 'buffer';
-// @ts-ignore
-if (typeof global !== 'undefined' && !(global).Buffer) (global).Buffer = Buffer;
-
-import { registerRootComponent } from 'expo';
-import App from './App';
-registerRootComponent(App);
